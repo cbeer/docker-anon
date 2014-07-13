88b204dde0469c5073d54d1f6e60001f85f37ec1
Chris Beer <chris@cbeer.info>
2014-07-13 09:48:37 -0700

    initial commit

---
+++ env-based-config.js
@@ -0,0 +1,17 @@
+var ranges = {}
+
+process.env.RANGES.split(";").map(function(r) {
+  var arr = r.split(":", 2);
+  var name = arr[0];
+  var org_range = arr[1];
+  ranges[name] = org_range.split(",").map(function(r) { return r.split("-"); });
+});
+
+module.exports = {
+  "nick": process.env.IRC_NICK,
+  "consumer_key": process.env.CONSUMER_KEY,
+  "consumer_secret": process.env.CONSUMER_SECRET,
+  "access_token": process.env.ACCESS_TOKEN,
+  "access_token_secret": process.env.ACCESS_TOKEN_SECRET,
+  "ranges": ranges
+}
